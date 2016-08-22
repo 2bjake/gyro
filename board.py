6a32451c2add70563ee5f146e8d1f0b964bea9b0
2bjake <fosterjake@gmail.com>
2016-08-22 17:42:20 -0500

    make 0,0 the bottom left

--- board.py
+++ board.py
@@ -6,6 +6,8 @@ class Board:
         self.width = width
         self.height = height
         self.blockMatrix = [[blocks.Block() for y in range(height)] for x in range(width)]
+        self.blockMatrix[0][0] = blocks.GroundBlock()
+        self.blockMatrix[width - 1][height - 1] = blocks.EmptyBlock()
 
     def render(self, screen, block_size):
         for x in range(self.width):
@@ -13,5 +15,5 @@ class Board:
                 self._render_block(screen, x, y, block_size)
 
     def _render_block(self, screen, block_x, block_y, block_size):
-        rect = ((block_size * block_x, block_size * block_y), (block_size, block_size));
+        rect = ((block_size * block_x, block_size * (self.height - block_y - 1)), (block_size, block_size));
         self.blockMatrix[block_x][block_y].render(screen, rect)
